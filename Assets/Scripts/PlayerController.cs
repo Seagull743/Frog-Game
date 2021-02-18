0daf3b8aa7dd8eb395e935ae25d4201b2398f29e
Sean <seangullery@gmail.com>
2021-02-18 15:16:57 +1300

    Camera & Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,10 +21,14 @@ public class PlayerController : MonoBehaviour
     public float jumpBufferLength;
     private float jumpBufferCount;
 
-   
+
+
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+
+       
     }
 
     private void FixedUpdate()
@@ -33,41 +37,25 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
-     void Update()
+    void Update()
     {
+
+
+
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpBufferCount = jumpBufferLength;
-        }
-        else
-        {
-            jumpBufferCount -= Time.deltaTime;
-        }
-
-        if(moveInput > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else if(moveInput < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-
-        if(jumpBufferCount >= 0 && isGrounded == true)
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             isjumping = true;
             jumptimeCounter = jumptime;
             rb.velocity = Vector2.up * jumpForce;
-            jumpBufferCount = 0;
         }
 
-        if(Input.GetKey(KeyCode.Space) && isjumping == true)
+
+        if (Input.GetKey(KeyCode.Space) && isjumping == true)
         {
-            if(jumptimeCounter > 0)
+
+            if (jumptimeCounter > 0)
             {
                 rb.velocity = Vector2.up * jumpForce;
                 jumptimeCounter -= Time.deltaTime;
@@ -76,12 +64,12 @@ public class PlayerController : MonoBehaviour
             {
                 isjumping = false;
             }
-            
-        }
 
+        }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isjumping = false;
         }
     }
+        
 }
---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+   
+
+   
+
+
+    public float jumpValue = 0.0f;
+    public bool canJump = true;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+       
+        if(jumpValue == 0.0f && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        }
+      
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && canJump)
+        {
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        }
+
+        
+        if (Input.GetKey(KeyCode.Space) && isGrounded && canJump)
+        {
+            jumpValue += 0.1f;
+        }
+
+        if (jumpValue >= 15f && isGrounded)
+        {
+            float tempx = moveInput * speed;
+            float tempy = jumpValue;
+            rb.velocity = new Vector2(tempx, tempy);
+            Invoke("ResetJump", 0.2f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(moveInput * speed, jumpValue);
+                jumpValue = 0.0f;
+            }
+        
+        {
+            canJump = true;
+        }
+        
+        
+
+    
+      }
+
+    private void ResetJump()
+    {
+        canJump = false;
+        jumpValue = 0;
+    }
+
+}
+ 
+
