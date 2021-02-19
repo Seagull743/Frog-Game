1b6a033e832296b1fa87392c45e696c8a4e7f4be
Dan <DucatiDan@outlook.com>
2021-02-19 15:20:53 +1300

    Frog?

---
+++ Assets/Dans Stuff/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
